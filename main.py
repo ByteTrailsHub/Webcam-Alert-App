6da5e294bf32c7976d15ad185c57c8bd11c8f56c
ByteTrailsHub <bytetrail24@gmail.com>
2025-03-07 08:03:19 +0700

    implement threading

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import glob
 import cv2
 import time
+import os
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -9,6 +11,11 @@ time.sleep(1)
 first_frame = None
 status_list =[]
 count = 1
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 while True:
     status = 0
@@ -46,7 +53,13 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+
 
     print(status_list)
 
@@ -57,3 +70,5 @@ while True:
         break
 
 video.release()
+
+clean_thread.start()
